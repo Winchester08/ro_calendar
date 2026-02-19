1878ecce83f76d103383d79c28b7c8e4e541bf69
RGP <ro@RGPs-MacBook-Pro.local>
2026-02-19 09:37:18 -0600

    Adding event model

--- calendario/models.py
+++ calendario/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class evento(models.Model):
+    fecha = models.CharField(max_length=30, verbose_name='Fecha Evento')
+    hora = models.CharField(max_length=10, verbose_name='Hora Evento')
+    descripcion = models.TextField(max_length=150, verbose_name='Descripcion')
+    estado = models.CharField(max_length=20, verbose_name='Esatus')
